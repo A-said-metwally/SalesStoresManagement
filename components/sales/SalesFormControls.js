76c0be71f868c462a8be44c5632f572f44aab5ec
Ahmed Said <86842879+A-said-metwally@users.noreply.github.com>
2022-10-21 09:58:01 +0200

    first commit

--- components/sales/SalesFormControls.js
+++ components/sales/SalesFormControls.js
@@ -4,26 +4,26 @@ function SalesFormControls({total, payment, setpayment}) {
 
   return (
     <div className=' h-full pb-3'>
-      <form className="flex flex-col space-y-4 h-full ">
-          <div className='flex  flex-col h-1/3'>
-              <label htmlFor="total" className=' w-28 text-xl font-semibold text-gray-700 ' >Total</label>
-              <input type="number" name='total' placeholder='total' value={total} 
-                  className="flex-1 border-2 border-blue-400 focus:outline-none p-2   text-center
+      <form className="flex flex-col space-y-4 h-full overflow-scroll  ">
+          <div className='flex  flex-col flex-1 '>
+              <label htmlFor="total" className=' text-xl font-semibold text-gray-700 ' >Total</label>
+              <input type="number" name='total' placeholder='total' value={-total} 
+                  className="flex-1 border-2 border-blue-400 focus:outline-none p-0   text-center
                   shadow-md  rounded-md text-2xl"
               />
           </div>
-          <div className='flex flex-col h-1/3'>
-              <label htmlFor="payment" className='w-28 text-xl font-semibold text-gray-700 ' >Payment</label>
+          <div className='flex flex-col flex-1'>
+              <label htmlFor="payment" className=' text-xl font-semibold text-gray-700 ' >Payment</label>
               <input type="number" name='payment' placeholder='payment' value={payment} 
-                  className="flex-1 border-2 border-blue-400 focus:outline-none p-2   text-center
+                  className="flex-1 border-2 border-blue-400 focus:outline-none p-0   text-center
                   shadow-md  rounded-md text-2xl"
                   onChange={(e)=>{ setpayment(parseInt(e.target.value))}}
               />
           </div>
-          <div className='flex flex-col h-1/3'>
-              <label htmlFor="remained" className='w-28 text-xl font-semibold text-gray-700 ' >Remaind</label>
+          <div className='flex flex-col flex-1'>
+              <label htmlFor="remained" className=' text-xl font-semibold text-gray-700 ' >Remaind</label>
               <input type="number" name='remained' placeholder='remained' value={total - payment} 
-                  className="flex-1 border-2 border-blue-400 focus:outline-none p-2   text-center
+                  className="flex-1 border-2 border-blue-400 focus:outline-none p-0   text-center
                   shadow-md  rounded-md text-2xl"
               />
           </div>
